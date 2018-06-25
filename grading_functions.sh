9f3a5962b735db80b74fdf885383fa8a3d274833
Matthew Magnusson <mfm2@unh.edu>
2018-06-25 15:40:30 -0400

    Update

--- grading_functions.sh
+++ grading_functions.sh
@@ -1,3 +1,16 @@
 get_file_lines(){
   cat $1  | wc -l
+
+}
+
+format_assert_error(){
+    echo Expected $1 Got $2
+}
+
+assert_int_equals(){
+   if [ "$1" -eq "$2" ]; then
+      echo true
+   else
+      format_assert_error $1 $2
+   fi
 }
